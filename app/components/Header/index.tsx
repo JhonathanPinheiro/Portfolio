a251e1c201dee1ecb56532078bdd265e573c7361
Jhonathan Pinheiro <jhonathancpinheiro@gmail.com>
2024-01-21 16:06:58 +0000

    feat: add header

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import Image from "next/image"
+import Link from "next/link"
+import { NavItem } from "./nav-itens"
+
+const NAV_ITENS = [
+    {
+        label: 'Home',
+        href: '/'
+    },
+    {
+        label: 'Projetos',
+        href: '/projects'
+    }
+]
+
+export const Header = () => {
+    return (
+        <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center">
+            <div className="container flex items-center justify-between">
+                <Link href='/'>
+                    <Image
+                    width={58}
+                    height={49}
+                    src='/images/icons/project-title-icon.svg'
+                    alt="Logo"
+                    />
+                </Link>
+
+                <nav className="flex items-center gap-4 sm:gap-10">
+                    {
+                        NAV_ITENS.map(item => (
+                            <NavItem key={item.label} {...item} />
+                        ))
+                    }
+                </nav>
+            </div>
+        </header>
+    )
+}
